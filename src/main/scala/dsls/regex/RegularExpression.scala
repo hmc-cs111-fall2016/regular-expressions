81a37e621dc0c04db83cf4a2f648261b71afc457
Michael Sheely <msheely@hmc.edu>
2016-10-05 22:49:00 -0700

    Finished assignment

--- src/main/scala/dsls/regex/RegularExpression.scala
+++ src/main/scala/dsls/regex/RegularExpression.scala
@@ -1,5 +1,10 @@
 package dsls.regex
 
+// Allows for the conversion of strings and chars to Regexes
+import scala.language.implicitConversions
+// Allows for star to be written as <*> (syntactic sugar)
+import scala.language.postfixOps
+
 /**
  * Modify this file to implement an internal DSL for regular expressions. 
  * 
@@ -11,6 +16,23 @@ package dsls.regex
 abstract class RegularExpression {
   /** returns true if the given string matches this regular expression */
   def matches(string: String) = RegexMatcher.matches(string, this)
+  /** returns a union of two regular expression */
+  def ||(other: RegularExpression) = Union(this, other)
+  /** returns the concatenation of two regular expressions */
+  def ~(other: RegularExpression) = Concat(this, other)
+  /** returns the stared version of the regular expression */
+  def <*>() = Star(this)
+  /** returns a regex which requires one copy of the regex
+      followed by any number of copies following (via Star) */
+  def <+>() = Concat(this, Star(this))
+  /** returns a regex which is n concatenations of the regex */
+  def apply(n: Int): RegularExpression = {
+    if (n == 1) {
+      this
+    } else {
+      Concat(this, this {n - 1})
+    }
+  }
 }
 
 /** a regular expression that matches nothing */
@@ -34,3 +56,16 @@ case class Concat(val left: RegularExpression, val right: RegularExpression)
  *  expression
  */
 case class Star(val expression: RegularExpression) extends RegularExpression
+
+/** Ensure that we can perform implicit conversions 
+ *  from chars and strings to our RegularExpressions
+ */
+object RegularExpressionImplicit {
+  implicit def char2Literal(lit: Char) = new Literal(lit)
+  implicit def string2Regex(str: String): RegularExpression = {
+    str.foldLeft(EPSILON: RegularExpression)(
+        (regex, c) => Concat(regex, Literal(c)))
+  }
+}
+
+
