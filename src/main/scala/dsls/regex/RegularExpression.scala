592d9ebbbb34cd2641374a0421ed0b46fb1d5e6b
aromm <aromm@g.hmc.edu>
2016-10-05 23:35:52 -0700

    Completed assignment

--- src/main/scala/dsls/regex/RegularExpression.scala
+++ src/main/scala/dsls/regex/RegularExpression.scala
@@ -1,5 +1,8 @@
 package dsls.regex
 
+//we should use htis line to enable implicit conversions
+import scala.language.implicitConversions
+import LiteralImplicits._
 /**
  * Modify this file to implement an internal DSL for regular expressions. 
  * 
@@ -11,26 +14,83 @@ package dsls.regex
 abstract class RegularExpression {
   /** returns true if the given string matches this regular expression */
   def matches(string: String) = RegexMatcher.matches(string, this)
+  def ||(other: RegularExpression) = Union(this, other)
+  def ~(other: RegularExpression) = Concat(this, other)
+  def <*> = Star(this)
+  def <+> = Concat(this, Star(this))
+
+  // for making a {n} method I was very confused how to do it 
+  // I was trying to create a different Literal class that could
+  // accept two parameters (but would give a defaut value for n)
+  // but realized that didn't seem right. I also tried making an implicit
+  // method that would take a char and a num or a literal and a num and return
+  // the proper RegularExpression. I couldnt get this to work. Eventually stack
+  // overflow http://stackoverflow.com/questions/3551366/can-i-overload-paranthesis-in-scala
+  // led me to the apply method in order to overload the braces
+  // I am not sure if this is the proper way but it passed my very singular
+  // test. (when I did for all n: Int, the loop took too long to run so its
+  // a simple test.)
+  def apply(num: Int): RegularExpression = {
+    var n: RegularExpression = EPSILON
+    var count = num
+    while(count > 0){
+        n = Concat(this, n)
+        count = count - 1
+    }
+    n
+  }
+  // {n} will need to use the curried function maybe
+
 }
 
+
 /** a regular expression that matches nothing */
-object EMPTY extends RegularExpression
+object EMPTY extends RegularExpression 
 
 /** a regular expression that matches the empty string */
-object EPSILON extends RegularExpression
+object EPSILON extends RegularExpression 
 
 /** a regular expression that matches a literal character */
-case class Literal(val literal: Char) extends RegularExpression
+case class Literal(literal: Char) extends RegularExpression 
+
+
+// or a char and num to literal
+
+// Literal Companion class for implicitConversions
+object LiteralImplicits {
+    implicit def char2literal(literal: Char): Literal = Literal(literal)
+
+    implicit def string2regexp(literal: String): RegularExpression = {
+        if (literal == ' ')
+            EPSILON
+        else
+            Concat(Literal(literal.charAt(0)), string2regexp(literal.substring(1)))
+
+    }
+
+    // implicit def litandnum2regexp(literal: Literal, num: Int): RegularExpression = {
+    //     if (num == 0)
+    //         EPSILON
+    //     else
+    //         Concat(literal, litandnum2regexp(literal, num - 1))
+    // }
+}
 
 /** a regular expression that matches either one expression or another */
 case class Union(val left: RegularExpression, val right: RegularExpression) 
-  extends RegularExpression
+  extends RegularExpression 
+
 
 /** a regular expression that matches one expression followed by another */
 case class Concat(val left: RegularExpression, val right: RegularExpression) 
-  extends RegularExpression
-  
+  extends RegularExpression 
+
+
 /** a regular expression that matches zero or more repetitions of another 
  *  expression
  */
 case class Star(val expression: RegularExpression) extends RegularExpression
+
+
+
+
--- src/test/scala/dsls/regex/RegexCheck.scala
+++ src/test/scala/dsls/regex/RegexCheck.scala
@@ -2,6 +2,8 @@ package dsls.regex
 
 import org.scalacheck._
 import Prop.forAll
+import scala.language.implicitConversions
+import LiteralImplicits._
 
 /**
  * These tests are to make sure that the pattern-matching algorithm works.
@@ -12,14 +14,31 @@ object RegexCheck extends Properties("Regex") {
   property("empty") = forAll { s: String ⇒ !(EMPTY matches s) }
   
   property("literals") = forAll { c: Char ⇒ Literal(c) matches c.toString }
-  
+
+  property("stringLiterals") = forAll { (c1: Char, c2: Char) => 
+    val pattern = c1.toString + c2.toString 
+    pattern matches (c1.toString + c2.toString)}
+
+  property("charImplicit") = forAll {c: Char =>
+    val pattern = c
+    pattern matches c.toString}
+
   property("concat") = forAll { (c1: Char, c2: Char) ⇒ 
     Concat(Literal(c1), Literal(c2)) matches (c1.toString + c2.toString) 
   }
   
+  property("concat~") = forAll {(c1: Char, c2: Char) =>
+    val pattern = c1 ~ c2
+    pattern matches (c1.toString + c2.toString) }
+
   property("union") = forAll { (c1: Char, c2: Char) ⇒ 
     val pattern = Union(Literal(c1), Literal(c2)) 
     (pattern matches c1.toString) && (pattern matches c2.toString)  
+  }
+
+  property("union||") = forAll { (c1: Char, c2: Char) =>
+    val pattern = Literal(c1) || Literal(c2)
+    (pattern matches c1.toString) && (pattern matches c2.toString)
   }
   
   property("star") = forAll { c: Char ⇒ 
@@ -27,7 +46,26 @@ object RegexCheck extends Properties("Regex") {
     (pattern matches "") && (pattern matches c.toString) && 
       (pattern matches c.toString * 3) 
   }
+
+  property("star<*>") = forAll { c: Char => 
+    val pattern = (Literal(c) <*>)
+    (pattern matches "") && (pattern matches c.toString) &&
+    (pattern matches c.toString * 3)
+  }
   
+  property("<+>") = forAll{ c: Char =>
+    val pattern = (c <+>)
+    (pattern matches c.toString) && (pattern matches c.toString * 3) &&
+    !(pattern matches "")
+  }
+
+  property("{n}") = forAll{ c: Char =>
+    val pattern = (c{3})
+    pattern matches c.toString * 3
+
+  }
+
+
   property("epsilon*") = forAll { c: Char ⇒ 
     !(Star(EPSILON) matches c.toString)
   }
